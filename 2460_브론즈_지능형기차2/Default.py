aa41f43ec503a447afeed339009a87738b5ef16f
ZANDHIFORCE <91401488+ZANDHIFORCE@users.noreply.github.com>
2024-01-26 06:58:17 +0000

    2460_브론즈_지능형기차2 solved

--- 2460_브론즈_지능형기차2/Default.py
+++ 2460_브론즈_지능형기차2/Default.py
@@ -4,7 +4,7 @@ total = 0
 max = 0
 
 for _ in range(10):
-  input_str = input()#file.readline()
+  input_str = input() #file.readline()
   pout, pin = map(int, input_str.split(" "))
   total = total - pout + pin
   if total>max:
