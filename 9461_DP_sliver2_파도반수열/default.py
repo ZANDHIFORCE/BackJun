b686377e19305bb9f620419929a1da6952368129
ZANDHIFORCE <91401488+ZANDHIFORCE@users.noreply.github.com>
2025-03-03 12:00:41 +0900

    DP 9461 sliver complete

---
+++ 9461_DP_sliver2_파도반수열/default.py
@@ -0,0 +1,18 @@
+#1 1 1 2 2 3 "4" 5 7 9 "12" 16 21
+#a[i] = a[i-1] + a[i-5]
+
+a = [0,1,1,1,2]
+
+repeat_num = int(input())
+input_list = []
+max = 0
+for i in range(repeat_num):
+    element = int(input())
+    max = element if max < element else max
+    input_list.append(element)
+    
+for i in range(5,max+1):
+    a.append(a[i-1]+a[i-5])
+    
+for x in input_list:
+    print(a[x])
