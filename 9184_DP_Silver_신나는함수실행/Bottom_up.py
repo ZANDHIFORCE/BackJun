00ef446fca6790180c658804fa5c282f05deb5f7
ZANDHIFORCE <91401488+ZANDHIFORCE@users.noreply.github.com>
2025-03-05 03:24:51 +0900

    1

--- 9184_DP_Silver_신나는함수실행/Bottom_up.py
+++ 9184_DP_Silver_신나는함수실행/Bottom_up.py
@@ -3,7 +3,7 @@
 #     if a<=0 or b<= 0 or c<= 0:
 #         return 1
 #     if a>20 or b>20 or c>20:
-#         return w(20,20,20)
+#         return w(20,20,20)1
 #     if a<b and b<c:
 #         return w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c)
     
