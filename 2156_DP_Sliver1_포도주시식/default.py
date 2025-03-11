bbd25b4d10626fe20cf29f8e4bdba5e4d73258e2
ZANDHIFORCE <91401488+ZANDHIFORCE@users.noreply.github.com>
2025-03-11 16:59:28 +0900

    modified 2156

--- 2156_DP_Sliver1_포도주시식/default.py
+++ 2156_DP_Sliver1_포도주시식/default.py
@@ -12,14 +12,15 @@ if N<2:
 
 dp[0][0] = wine_list[0]
 dp[1][0] = wine_list[1]
-dp[1][1] = win_list[1]+dp[0][0]
+dp[1][1] = wine_list[1]+dp[0][0]
 
 max_value = dp[1][1]
 
-for i in range(N):
-    dp[i][0] = wine_list + max(dp[i-2])
-    dp[i][1] = wine_list + dp[i-1][1]
-    max_value = max(dp[i], max_value)
+for i in range(2,N):
+    dp[i][0] = wine_list[i] + max(dp[i-2])
+    dp[i][1] = wine_list[i] + dp[i-1][0]
+    
+max_value = max(max((dp[N-1]+dp[N-2])), max_value)
 
 print(max_value)
 
