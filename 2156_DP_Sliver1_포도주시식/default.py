2bab332a8019af51c7fd13bb9bf367241bd21b99
ZANDHIFORCE <91401488+ZANDHIFORCE@users.noreply.github.com>
2025-03-12 16:42:31 +0900

    fixing

--- 2156_DP_Sliver1_포도주시식/default.py
+++ 2156_DP_Sliver1_포도주시식/default.py
@@ -6,11 +6,15 @@ for i in range(N):
 
 dp = [[0 for j in range(2)] for i in range(N)]
 
-if N<2:
+if N==1:
     print(wine_list[0])
+    quit()
+elif N==2:
+    print(sum(wine_list))
     quit()
 
 dp[0][0] = wine_list[0]
+dp[0][1] = 0
 dp[1][0] = wine_list[1]
 dp[1][1] = wine_list[1]+dp[0][0]
 
@@ -20,7 +24,7 @@ for i in range(2,N):
     dp[i][0] = wine_list[i] + max(dp[i-2])
     dp[i][1] = wine_list[i] + dp[i-1][0]
     
-max_value = max(max((dp[N-1]+dp[N-2])), max_value)
+max_value = max(dp[N-1]+dp[N-2]+ dp[N-3])
 
 print(max_value)
 
