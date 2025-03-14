0384fe7dd19c1c665501ce0be343e31b801be298
ZANDHIFORCE <91401488+ZANDHIFORCE@users.noreply.github.com>
2025-03-15 00:34:19 +0900

    version2

---
+++ 2156_DP_Sliver1_포도주시식/version2.py
@@ -0,0 +1,28 @@
+N = int(input())
+
+wine = []
+
+for i in range(N):
+    wine.append(int(input()))
+
+if N ==1:
+    print(wine[0])
+elif N ==2:
+    print(wine[0]+wine[1])
+else:
+    dp = [[0,0] for i in range(N)]
+
+    #there are two state
+    #1. OXO first sip dp[i][0] = wine[i] + max(dp[i-2][0],dp[i-2][1])
+    #2. XOO second sip dp[i][1] = wine[i] + dpo[i-1][0]
+
+    for i in range(3,N):
+        dp[i][0] = wine[i] + max(dp[i-2][0],dp[i-2][1])
+        dp[i][1] = wine[i] + dpo[i-1][0]
+
+    #Last sip end 
+    #OXO XOO
+    #second to last sip end
+    #XOX OOX
+
+    print(max(dp[N-1][0],dp[N-1][1],dp[N-2][0],dp[N-2][1]))
