b8f37f7a8e042b0f6b6907ca62596598a666670a
ZANDHIFORCE <91401488+ZANDHIFORCE@users.noreply.github.com>
2025-03-15 19:03:51 +0900

    version2 add condition

--- 2156_DP_Sliver1_포도주시식/version2.py
+++ 2156_DP_Sliver1_포도주시식/version2.py
@@ -9,6 +9,11 @@ if N ==1:
     print(wine[0])
 elif N ==2:
     print(wine[0]+wine[1])
+elif N ==3:
+    #XOO
+    #OXO
+    #OOX
+    print(max(wine[1]+wine[2], wine[0]+wine[2], wine[0]+wine[1]))
 else:
     dp = [[0,0] for i in range(N)]
     dp[0][0], dp[0][1] = wine[0], wine[0]
@@ -19,7 +24,7 @@ else:
     #2. -> XOO second sip dp[i][1] = wine[i] + dpo[i-1][0]
 
     for i in range(3,N):
-        dp[i][0] = wine[i] + max(dp[i-2][0],dp[i-2][1])
+        dp[i][0] = max( wine[i] + max(dp[i-2][0],dp[i-2][1]) , wine[i]+ max(dp[i-3][1],dp[i-3][1]) )
         dp[i][1] = wine[i] + dp[i-1][0]
 
     #Last sip end 
@@ -27,4 +32,11 @@ else:
     #second to last sip end
     #XOX OOX
 
-    print(max(dp[N-1][0],dp[N-1][1],dp[N-2][0],dp[N-2][1]))
+    #[!]additional solution
+    #ERROR
+    #OXXOXXO 100 1 1 100 1 1 100 can be best
+    #can XXX be the best?
+    #OXXXO => OXOXO will be more effective so NEGATIVE
+    #We have to consider OXXO okay
+
+    print(max(dp[N-1][0],dp[N-1][1],dp[N-2][0],dp[N-2][1],dp[N-3][0],dp[N-3][1]))
